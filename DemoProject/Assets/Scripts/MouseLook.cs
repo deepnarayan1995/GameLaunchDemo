bdb6e100b922e9c409f8a7efabf20e3485555a9c
deepnarayan1995 <56741670+deepnarayan1995@users.noreply.github.com>
2020-01-29 01:18:36 +0530

    MouseControl created

---
+++ DemoProject/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    float mousex, mousey;
+    public float sensitivity = 100f;
+    public Transform playerbody;
+    float xRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        mousex = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mousey = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        playerbody.Rotate(Vector3.up * mousex);
+        xRotation -= mousey;
+        xRotation = Mathf.Clamp(xRotation, -90f, 60f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
