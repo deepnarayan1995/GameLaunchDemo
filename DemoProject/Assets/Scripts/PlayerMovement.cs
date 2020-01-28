a9ec0aee1ce864c8cb7653bc6bea43e834f2a434
deepnarayan1995 <56741670+deepnarayan1995@users.noreply.github.com>
2020-01-29 01:48:25 +0530

    PlayerMovement Initiated

--- DemoProject/Assets/Scripts/PlayerMovement.cs
+++ DemoProject/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float xval, zval;
+    public float speed = 6f;
+    public CharacterController controller;
+    public float gravity = 9.8f, jumpheight = 3f;
+    Vector3 velocity;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        xval = Input.GetAxis("Horizontal");
+        zval = Input.GetAxis("Vertical");
+
+        Vector3 moving = transform.right * xval + transform.forward * zval;
+        controller.Move(moving * speed * Time.deltaTime);
+
     }
 }
